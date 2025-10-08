8f34117407a4cae2ceea6a824a55e973d737a280
Moises Velasquez <moises007@gmail.com>
2025-10-07 22:53:01 -0500

    data fetching

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,10 @@
-export default function PokemonsPage() {
-  return (
-    <div>
-      <h1>Hello Page Pokemons</h1>
-    </div>
-  );
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const res = await data.json();
+  return res;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+  return <div>{JSON.stringify(pokemons)}</div>;
 }
